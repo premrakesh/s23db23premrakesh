34c34eeeb0d5e1663343a4f858b41fd149914684
premrakesh <rakesh14082001@gmail.com>
2023-11-20 15:32:37 -0600

    lab 12

--- controllers/mountains.js
+++ controllers/mountains.js
@@ -86,7 +86,10 @@ exports.mountains_view_one_Page = async function(req, res) {
     }
    };
 
-   exports.mountains_create_Page = function(req, res) {
+   // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.mountains_create_Page = function(req, res) {
     console.log("create view")
     try{
     res.render('mountainscreate', { title: 'Mountains Create'});
@@ -95,7 +98,8 @@ exports.mountains_view_one_Page = async function(req, res) {
     res.status(500)
     res.send(`{'error': '${err}'}`);
     }
-   };
+    };
+    
 // VIEWS
 // Handle a show all view
 exports.mountains_view_all_Page = async function(req, res) {
@@ -108,3 +112,32 @@ res.status(500);
 res.send(`{"error": ${err}}`);
 }
 };
+
+// Handle a delete one view with id from query
+exports.mountains_delete_Page = async function(req, res) {
+console.log("Delete view for id " + req.query.id)
+try{
+result = await mountains.findById(req.query.id)
+res.render('mountainsdelete', { title: 'Mountains Delete', toShow:
+result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+
+// Handle building the view for updating a costume.
+// query provides the id
+exports.mountains_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await mountains.findById(req.query.id)
+    res.render('mountainsupdate', { title: 'Mountains Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/mountains.js
+++ routes/mountains.js
@@ -6,4 +6,13 @@ var router = express.Router();
 router.get('/', mountains_controlers.mountains_view_all_Page );
 
 router.get('/detail', mountains_controlers.mountains_view_one_Page);
+
+router.get('/create', mountains_controlers.mountains_create_Page);
+
+/* GET create update page */
+router.get('/update', mountains_controlers.mountains_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', mountains_controlers.mountains_delete_Page);
+
 module.exports = router;
